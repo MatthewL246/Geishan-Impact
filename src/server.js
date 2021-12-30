2e6f0b01c5b5fcbc9dc216a371002fe70d35860f
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2021-12-30 09:42:02 -0500

    Use the *actual* right subdomain

--- src/server.js
+++ src/server.js
@@ -43,8 +43,8 @@ app.use(express.urlencoded({
 }));
 
 // Create subdomains
-logger.info('[WUP] Creating \'geisha-wup.cdn.shop\' subdomain');
-app.use(subdomain('geisha-wup.cdn.shop', ROUTERS.WUP));
+logger.info('[WUP] Creating \'geisha-wup.cdn\' subdomain');
+app.use(subdomain('geisha-wup.cdn', ROUTERS.WUP));
 
 // Setup routes
 logger.info('[WUP] Applying imported routes');
